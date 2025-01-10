d05d7077a1f9ce1b11fd4e2f2b5880dcb82ef1d7
Robin <balyanrobin@gmail.com>
2025-01-10 16:35:03 -0500

    chore: script with user inputs

--- cumulusStepsRunner.ts
+++ cumulusStepsRunner.ts
@@ -6,9 +6,12 @@
 
 import fs from "fs";
 import path from "path";
+import readline from "readline";
 import { CumulusSteps } from "./cumulusSteps";
 
 const targetRepoPath = process.argv[2];
+const nonInteractive = process.argv[3] === "--non-interactive";
+
 if (!fs.existsSync(targetRepoPath)) {
   console.error(`Path ${targetRepoPath} does not exist`);
   process.exit(1);
@@ -19,6 +22,29 @@ const steps = cumulusSteps.getSteps();
 console.log("Following steps are added in the list for execution:");
 steps.forEach((step, indx) => {
   console.log(`  ${indx + 1}. ${step}`);
+});
+
+if (nonInteractive) {
+  // cumulusSteps.runSteps();
+  console.log("Executing steps in non-interactive mode");
+  process.exit(0);
+}
+
+// read user input
+const readUserInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readUserInput.question("Do you want to execute all steps? (Y/n)", (answer) => {
+  // handle yes or enter key
+  if (answer.toLowerCase() === "y" || answer === "") {
+    // cumulusSteps.runSteps();
+    console.log("Exiting after executing");
+  } else {
+    console.log("Exiting without executing steps");
+  }
+  readUserInput.close();
 });
 
 // cumulusSteps.runSteps();
